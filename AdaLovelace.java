66409037776a1be50268539e235c1f7a89eb3865
Enki Layman <layman212@gmail.com>
2025-02-17 23:48:43 -0500

    learned the intellisense shortcut for print statements

--- AdaLovelace.java
+++ AdaLovelace.java
@@ -3,5 +3,7 @@ public class AdaLovelace {
         System.out.println("Once upon a time");
         System.out.println("There was");
         System.out.println("A program");
+        System.out.println();
+        System.out.println();
     }
 }
