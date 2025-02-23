08a8c4258555fc06155860631a21480d4dbf27be
Enki Layman <layman212@gmail.com>
2025-02-23 02:53:05 -0500

    You can print varibales to the console

---
+++ PrintVariables.java
@@ -0,0 +1,11 @@
+public class PrintVariables {
+    public static void main(String[] args) {
+        int num = 42;
+        String string = "Hey";
+        char letter = 'A';
+
+        System.out.println(num);
+        System.out.println(string);
+        System.out.println(letter);
+    }
+}
